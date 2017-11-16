1568cf6a229da3bd891e09fef6fb4bfcbaeab6d3
yumcyawiz <sergeyinverse@gmail.com>
2017-11-16 21:20:44 +0900

    3sphere scene

--- main.cpp
+++ main.cpp
@@ -45,16 +45,27 @@ int main(int argc, char** argv) {
 
     Image *img = new Image(width, height);
     
-    Point3 camPos = Point3(0, 3, 11);
+    Point3 camPos = Point3(0, 1, 4);
     Vec3 camForward = normalize(Point3() - camPos);
     //Camera *cam = new PinholeCamera(camPos, camForward, 1.5f, 1.5f);
-    Camera *cam = new ThinLensCamera(camPos, camForward, 3.0f, 1.5f, Point3(0, 0, 0), 12.0f);
-    //Camera *cam = new EquidistantFisheyeCamera(camPos, camForward, 1.0f, 1.0f);
+    //Camera *cam = new ThinLensCamera(camPos, camForward, 3.0f, 1.5f, Point3(0, 0, 0), 12.0f);
+    Camera *cam = new EquidistantFisheyeCamera(camPos, camForward, 1.5f, 1.0f);
 
     Objects *objs = new Objects();
     Transform t = translate(Vec3(0, -1, 0))*scale(10.0, 1.0, 10.0);
     Transform t_inv = inverse(t);
     objs->add(new Plane(&t, &t_inv, new Diffuse(0.9f), new Checkerboard(new Mono(RGB(1.0f)), new Mono(RGB(0.1f)), 5.0f)));
+    Transform t2 = translate(Vec3(0, 0, 0));
+    Transform t2_inv = inverse(t2);
+    objs->add(new Sphere(&t2, &t2_inv, new Glass(1.4f), new ImageTexture("earth2.jpg"), 1.0f, -1.0f, 1.0f, 2*M_PI));
+    Transform t3 = translate(Vec3(3, 0, 0));
+    Transform t3_inv = inverse(t3);
+    objs->add(new Sphere(&t3, &t3_inv, new Glass(1.4f), new Mono(RGB(1.0f)), 1.0f, -1.0f, 1.0f, 2*M_PI));
+    
+    Transform t4 = translate(Vec3(-3, 0, 0));
+    Transform t4_inv = inverse(t4);
+    objs->add(new Sphere(&t4, &t4_inv, new Mirror(1.0f), new Mono(RGB(1.0f)), 1.0f, -1.0f, 1.0f, 2*M_PI));
+    /*
     Transform t2 = translate(Vec3(-5, -1, 0));
     Transform t2_inv = inverse(t2);
     objs->add(new Paraboloid(&t2, &t2_inv, new Diffuse(0.9f), new ImageTexture("uv_test.png"), 3.0f, 1.0f, 0.0f, 1.5f, 2*M_PI));
@@ -70,6 +81,7 @@ int main(int argc, char** argv) {
     Transform t6 = translate(Vec3(5, 0, 0));
     Transform t6_inv = inverse(t6);
     objs->add(new Sphere(&t6, &t6_inv, new Diffuse(0.9f), new ImageTexture("uv_test.png"), 1.0f, -1.0f, 1.0f, 2*M_PI));
+    */
 
     Sky *sky = new IBL("PaperMill_E_3k.hdr");
 
